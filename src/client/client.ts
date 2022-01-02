deaf0f6e0561b4810a3f5d40d6cd7e5c4412c201
sam <sam.leider@hotmail.com>
2022-01-01 22:00:24 -0500

    4 torus knot with different camera positions

--- src/client/client.ts
+++ src/client/client.ts
@@ -3,36 +3,71 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(
+
+const camera1 = new THREE.PerspectiveCamera(
   75,
-  window.innerWidth / window.innerHeight,
+  1,
   0.1,
-  1000
+  10
+);
+const camera2 = new THREE.OrthographicCamera(-1,1,1,-1, 0.1, 10);
+const camera3 = new THREE.OrthographicCamera(-1,1,1,-1, 0.1, 10);
+const camera4 = new THREE.OrthographicCamera(-1,1,1,-1, 0.1, 10);
+
+camera1.position.z = 2;
+camera2.position.y = 2;
+camera2.lookAt(new THREE.Vector3());
+camera3.position.x = -2;
+camera3.lookAt(new THREE.Vector3(0,0,0)); 
+camera4.position.z = 2;
+
+// canvasess
+const canvas1 = document.getElementById('c1') as HTMLCanvasElement;
+const canvas2 = document.getElementById('c2') as HTMLCanvasElement;
+const canvas3 = document.getElementById('c3') as HTMLCanvasElement;
+const canvas4 = document.getElementById('c4') as HTMLCanvasElement;
+
+const renderer1 = new THREE.WebGLRenderer(
+  {canvas: canvas1, antialias: true}
+);
+const renderer2 = new THREE.WebGLRenderer(
+  {canvas: canvas2, antialias: true}
+);
+const renderer3 = new THREE.WebGLRenderer(
+  {canvas: canvas3, antialias: true}
+);
+const renderer4 = new THREE.WebGLRenderer(
+  {canvas: canvas4, antialias: true}
 );
 
-camera.position.z = 2;
+renderer1.setSize(200, 200);
+renderer2.setSize(200, 200);
+renderer3.setSize(200, 200);
+renderer4.setSize(200, 200);
+// document.body.appendChild(renderer.domElement);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+new OrbitControls(camera1, renderer1.domElement);
 
-new OrbitControls(camera, renderer.domElement);
-
-const geometry = new THREE.BoxGeometry();
+const geometry = new THREE.TorusKnotGeometry(); //THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
 
 const cube = new THREE.Mesh(geometry, material);
+cube.scale.x = 0.5;
+cube.scale.y = 0.5;
+cube.scale.z = 0.5;
 
 scene.add(cube);
-window.addEventListener('resize', onWindowResize, false);
+console.dir(scene)
 
-function onWindowResize(){
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+// window.addEventListener('resize', onWindowResize, false);
 
-  render();
-}
+// function onWindowResize(){
+//   camera.aspect = window.innerWidth / window.innerHeight;
+//   camera.updateProjectionMatrix();
+//   renderer.setSize(window.innerWidth, window.innerHeight);
+
+//   render();
+// }
 
 function animate(){
   requestAnimationFrame(animate);
@@ -42,7 +77,10 @@ function animate(){
 }
 
 function render(){
-  renderer.render(scene, camera);
+  renderer1.render(scene, camera1);
+  renderer2.render(scene, camera2);
+  renderer3.render(scene, camera3);
+  renderer4.render(scene, camera4);
 }
 
 animate();
