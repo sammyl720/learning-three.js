1b8e043e3bbf4a4fede3355201a41d9443d1088c
sam <sam.leider@hotmail.com>
2022-01-01 22:24:13 -0500

    stats gui

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import Stats from 'three/examples/jsm/libs/stats.module';
 
 const scene = new THREE.Scene();
 
@@ -35,16 +36,23 @@ function onWindowResize(){
   render();
 }
 
-// function animate(){
-//   requestAnimationFrame(animate);
-//   cube.rotation.x += 0.01;
-//   cube.rotation.y += 0.01;
-//   render();
-// }
+const stats = Stats();
+document.body.appendChild(stats.dom);
+
+function animate(){
+     requestAnimationFrame(animate);
+
+    //  stats.begin()
+     cube.rotation.x += 0.01;
+     cube.rotation.y += 0.01;
+    //  stats.end()
+     render(); 
+
+     stats.update();
+}
 
 function render(){
   renderer.render(scene, camera);
 }
 
-render()
-// animate();
+animate();
